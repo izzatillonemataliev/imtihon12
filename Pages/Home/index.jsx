768b8762964be4fc99e4afa6d3e922629142619c
izzatillonemataliev <156827918+izzatillonemataliev@users.noreply.github.com>
2024-05-22 13:01:01 +0500

    pw

---
+++ Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div className="container">
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
+
